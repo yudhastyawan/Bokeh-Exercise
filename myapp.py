d6ba458432b0718461fcb53c3b21e1bddf5f69b9
yudhastyawan <39240883+yudhastyawan@users.noreply.github.com>
2018-11-03 20:29:28 +0700

    First Create
    some examples of Bokeh exercise

---
+++ myapp.py
@@ -0,0 +1,37 @@
+from random import random
+
+from bokeh.layouts import column
+from bokeh.models import Button
+from bokeh.plotting import figure, curdoc
+
+# create a plot and style its properties
+p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
+p.border_fill_color = 'black'
+p.background_fill_color = 'black'
+p.outline_line_color = None
+p.grid.grid_line_color = None
+
+# add a text renderer to our plot (no data yet)
+r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
+           text_baseline="middle", text_align="center")
+
+i = 0
+
+ds = r.data_source
+
+# create a callback that will add a number in a random location
+def callback():
+    global i
+
+    new_data = dict()
+    new_data['x'] = ds.data['x'] + [random()*70 + 15]
+    new_data['y'] = ds.data['y'] + [random()*70 + 15]
+    new_data['text'] = ds.data['text'] + [str(i)]
+    ds.data = new_data
+
+    i = i + 1
+
+button = Button(label="Press Me")
+button.on_click(callback)
+
+curdoc().add_root(column(button,p))
